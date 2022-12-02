c6fd67ffd8af57553106a33fb2cfd53e8e19117b
IsmaelBaena <ismael.baena.contacto@gmail.com>
2022-12-02 12:30:58 +0100

    Animacion aplicada a perfil

--- JavaScript/PathGeneratorAnimation.js
+++ JavaScript/PathGeneratorAnimation.js
@@ -1,43 +1,83 @@
-const REPETICIONES = 10;
-const SAMPLETITLE = "Sample Title", SAMPLETEXT = "Hola mundo guapo mucho bonito";
+const REPETICIONES = 3;
+const SAMPLETITLE = "Bootcamp en aprender a escribir Hola Mundo", SAMPLELOCATION = "Cuenca, Cuenca, ClM, España", SAMPLEDATE = "Hoy";
+const SAMPLETEXT = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut ipsam dolorum officiis voluptas eaque. Ut dolor ea nam culpa repellat inventore nemo recusandae quasi ratione quidem, asperiores, aspernatur quibusdam eum.";
 var isStart = true, isVisible = false;
 
-function generateDiv() {
+function generateDiv(timelineType) {
     for(var i = 0; i < REPETICIONES; i++) {
-        var div = document.createElement("div"), h2 = document.createElement("h2"), p = document.createElement("p");
+        var container = document.createElement("div"), 
+            date = document.createElement("div"), 
+            content = document.createElement("div"), 
+            h2 = document.createElement("h2"), 
+            h3 = document.createElement("h3"),  
+            p = document.createElement("p");
         
-        div.id = "point_"+ (i + 1);
-        if (isStart) { div.className = "point start" }
-        else { div.className = "point end" }
+        container.id = "point_" + timelineType + "_" + (i + 1);
+        if (isStart) { container.className = "container left" }
+        else { container.className = "container right" }
+
+        content.id = "child_of_point_" + timelineType + "_" + (i + 1)
+        content.className = "content";
+        
+        date.className = "date";
+        date.innerHTML = SAMPLEDATE;
+        
         h2.innerHTML = SAMPLETITLE;
+        h3.innerHTML = SAMPLELOCATION;
+        
         p.innerHTML = SAMPLETEXT;
 
         isStart = !isStart;
 
-        div.appendChild(h2);
-        div.appendChild(p);
-        document.getElementById("pathContent").appendChild(div);
+        content.appendChild(h2);
+        content.appendChild(h3);
+        content.appendChild(p);
+
+        container.appendChild(date);
+        container.appendChild(content);
+
+        document.getElementById(timelineType).appendChild(container);
+        document.getElementById(timelineType).style.transition = "height " + REPETICIONES + "s";
     }
 }
 
-function changeVisibilityDivs(num) {
+function changeVisibilityDivs(num, timeline) {
     return new Promise((resolve, reject) => {
         setTimeout(() => { 
-            resolve(document.getElementById("point_" + num))
+            resolve(document.getElementById("point_" + timeline + "_" + num))
         }, 1000 * num);
     })
 }
 
-$("#showPath").click(() => {
-        document.getElementById("pathContent").style.visibility = "visible";
-        document.getElementById("pathContent").style.height = (REPETICIONES * 100) + "px";
-        for (var i = 0; i < REPETICIONES; i++) {  
-            changeVisibilityDivs(i + 1).then((datos) => {
-                datos.style.visibility = "visible";
-                datos.style.width = "50%"; 
-            });
-        }
+function showTimeline (timeline) { 
+    document.getElementById(timeline).style.visibility = "visible";
+    var heigtSum = 0;
+    for (let i = 0; i < REPETICIONES; i++) {
+        var obj = document.getElementById("point_" + timeline + "_" + (i + 1));
+        if(obj.offsetHeight) { heigtSum += obj.offsetHeight; } 
+        else { heigtSum += obj.style.pixelHeight; }
+    }
+    for (var i = 0; i < REPETICIONES; i++) {  
+        changeVisibilityDivs((i + 1), timeline).then((datos) => {
+            datos.style.visibility = "visible";
+            datos.style.width = "50%"; 
+        });
+    }
+    document.getElementById(timeline).style.height = (heigtSum) + "px";
+}
+
+$(".triangulo").hover(() => {
+    setTimeout(() => { 
+        showTimeline("timeline");
+    }, 0);
+    setTimeout(() => { 
+        showTimeline("timeline_c");
+    }, 4000);
+    setTimeout(() => { 
+        showTimeline("timeline_f");
+    }, 8000);
 });
 
-generateDiv();
-
+generateDiv("timeline");
+generateDiv("timeline_c");
+generateDiv("timeline_f");

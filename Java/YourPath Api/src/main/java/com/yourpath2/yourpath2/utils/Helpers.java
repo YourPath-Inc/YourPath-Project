29cdda35cc39cea2a0ff2fddf85d25c7d6e28cb9
IsmaelBaena <ismael.baena.contacto@gmail.com>
2022-12-12 08:27:45 +0100

    Api Consulting Requests

--- Java/YourPath Api/src/main/java/com/yourpath2/yourpath2/utils/Helpers.java
+++
@@ -1,9 +0,0 @@
-package com.yourpath2.yourpath2.utils;
-
-import org.modelmapper.ModelMapper;
-
-public class Helpers {
-    public static ModelMapper ModelMapper(){
-        return new ModelMapper();
-    }
-}

efbcd1abaa1f17509ab9fe16f48c5f97337182de
IsmaelBaena <ismael.baena.contacto@gmail.com>
2022-12-14 10:50:15 +0100

    AdaptacionES

--- Java/Api/src/main/java/com/api/entities/Punto.java
+++ Java/Api/src/main/java/com/api/entities/Punto.java
@@ -3,6 +3,8 @@ import java.sql.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.Data;
@@ -12,6 +14,7 @@ import lombok.Data;
 @Table(name="punto")
 public class Punto {
 	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name="punto_id")
     private Long id;
     @Column(name="user_id")
--- JavaScript/PathUsefulness.js
+++ JavaScript/PathUsefulness.js
@@ -1,10 +1,12 @@
 import { showTimeline } from './Modules/PathAnimation.js';
 import { generateDiv } from './Modules/PathContentGenerator.js';
 import { Point } from './Modules/PointClass.js';
+import { getPointsByIdUser } from "./Modules/APIConsultor.js"
 
 const REPETICIONES = 3;
 const SAMPLETITLE = "Bootcamp en aprender a escribir Hola Mundo", SAMPLELOCATION = "Cuenca, Cuenca, ClM, España", SAMPLEDATE = "Hoy";
 const SAMPLETEXT = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut ipsam dolorum officiis voluptas eaque. Ut dolor ea nam culpa repellat inventore nemo recusandae quasi ratione quidem, asperiores, aspernatur quibusdam eum.";
+const STATES = ["DONE", "DOING", "TODO"];
 
 $(".triangulo").hover(() => {
     setTimeout(() => { 
@@ -18,13 +20,27 @@ $(".triangulo").hover(() => {
     }, 8000);
 });
 
-var arrayPoints = new Array(3);
+var points = await getPointsByIdUser(1);
+console.log(points);
+var timelinePoints = [], timelinePointsC = [], timelinePointsF = [];
 
-for (var i = 0; i < REPETICIONES; i++) {
-    var p = new Point(1, SAMPLETITLE, SAMPLEDATE, SAMPLEDATE, SAMPLETEXT, "state", SAMPLELOCATION);
-    arrayPoints[i] = p;
+for (var i = 0; i < points.length; i++) {
+    var point = points[i];
+    var p = new Point(point.user_id, point.titulo, point.init_date, point.end_date, point.state, point.location)
+    switch(points[i].state) {
+        case STATES[0]:
+            timelinePoints.push(p);
+            break;
+        case STATES[1]:
+            timelinePointsC.push(p);
+            break;
+        case STATES[2]:
+            timelinePointsF.push(p);
+            break;
+    }
 }
 
-generateDiv("timeline", arrayPoints);
-generateDiv("timeline_c", arrayPoints);
-generateDiv("timeline_f", arrayPoints);
+generateDiv("timeline", timelinePoints);
+if (timelinePointsC[0] != undefined) { generateDiv("timeline_c", timelinePointsC); }
+
+generateDiv("timeline_f", timelinePointsF);
